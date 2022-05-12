a43156cb8244483ccdbb63c13b60b4546141c11e
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2022-05-12 00:26:54 -0300

    Adding react table and creating GenericTable component

--- webNext/src/pages/AccessHistory.jsx
+++ webNext/src/pages/AccessHistory.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import { React, useMemo } from "react";
 
+// Components
+import GenericTable from "../components/GenericTable";
 import Layout from '../components/Layout';
 
 export default function AccessHistory() {
+    const columns = useMemo(
+        () => [
+            {
+                Header: "Histórico de violações",
+                columns: [
+                    {
+                        Header: "Data",
+                        accessor: "date"
+                    },
+                    {
+                        Header: "hora",
+                        accessor: "hour"
+                    },
+                    {
+                        Header: "Setor",
+                        accessor: "department"
+                    },
+                    {
+                        Header: "Violação",
+                        accessor: "maskIcon"
+                    }
+                ]
+            }
+        ],
+        []
+    );
+
     return (
         <Layout>
             Historico de acessos
+
+            <GenericTable
+                columns={columns}
+                data={[]}
+            />
         </Layout>
     )
 }
